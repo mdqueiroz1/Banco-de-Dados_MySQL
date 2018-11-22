50009dfecf7d7791fa47513d0d7df4f56c5b591f
Matheus D Queiroz <32309799+mdqueiroz1@users.noreply.github.com>
2018-11-21 23:59:28 -0200

    21/11/18

---
+++ ProjetoSIN220/script_insercao_projeto_BD.sql
@@ -0,0 +1,22 @@
+insert into instituto values
+  ('1', 'agrarias'),-- 1
+  ('2', 'engenharias'), -- 2
+  ('3', 'biologicas'), -- 3
+  ('4', 'exatas e da terra'), -- 4
+  ('5', 'humanas'), -- 5
+  ('6', 'sociais e aplicadas'), -- 6
+  ('7', 'saude');
+
+insert into curso values
+  ('1','6','Administração - Integral'),
+  ('2','6','Administração - Noturno'),
+  ('3','1','Agronomia - Integral'),
+  ('4','3','Ciências Biológicas'),
+  ('5','3','Ciência de Alimentos'),
+  ('6','2','Engenharia Civil'),
+  ('7','2','Engenharia de Produção'),
+  ('8','2','Engenharia de Produção'),
+  ('9','7','Nutrição'),
+  ('10','4','Quimica'),
+  ('11','4','Sistemas de Informação - Integral'),
+  ('12','4','Sistemas de Informação - Noturno');
